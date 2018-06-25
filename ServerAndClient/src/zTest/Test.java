fbbf9931deb0622823f3c22f2bda33cb2593547b
chenguanda <chenguanda@lakala.com>
2018-06-25 17:01:17 +0800

    删除bin

--- ServerAndClient/src/zTest/Test.java
+++ ServerAndClient/src/zTest/Test.java
@@ -1,32 +1,21 @@
 package zTest;
 
-import java.io.File;
-import java.util.Observer;
-
-import DAO.DBproxy;
-import DAO.DataBaseConnectionToAccess;
-import DAO.DataBaseConnectionToMysql;
-import overall.BuildClient;
-import overall.BuildServer;
 import overall.Builder;
 import overall.Diector;
-import overall.End;
 import overall.XMLUtil;
-import thread.GetFileFromEveryone;
-import thread.SendFileEveryone;
+
 
 public class Test {
 
 	public static void main(String[] args) {
 		try {
-	
-     Diector d=new Diector();
-     d.setBuilder((Builder)XMLUtil.getBean("Server"));
-     d.construct();
+			// spmething I want to change
+			Diector d = new Diector();
+			d.setBuilder((Builder) XMLUtil.getBean("Server"));
+			d.construct();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
 }
-	
